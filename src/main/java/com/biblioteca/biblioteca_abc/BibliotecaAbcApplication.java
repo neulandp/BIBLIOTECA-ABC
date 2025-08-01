f5b42fc983f10c7263dfa6758da4b7ed5cf30004
neulandp <neulandp074@gmail.com>
2025-08-01 20:03:28 -0300

    Adiciona arquivos do projeto inicial

---
+++ src/main/java/com/biblioteca/biblioteca_abc/BibliotecaAbcApplication.java
@@ -0,0 +1,13 @@
+package com.biblioteca.biblioteca_abc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BibliotecaAbcApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BibliotecaAbcApplication.class, args);
+	}
+
+}
